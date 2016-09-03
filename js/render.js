1152f9a41d99cf81be1690f726c297daeb9593e6
KENJU <ken901waga@gmail.com>
2016-09-03 10:11:42 +0900

    - Create components for Title and SubTitle

--- js/render.js
+++ js/render.js
@@ -32,9 +32,56 @@ var Section = React.createClass({
     render: function () {
         return (
             <section>
-
+                <Section.Title title={"Who I am"}/>
+                <Section.SubTitle title={"Contact"}/>
+                <Section.SubTitle title={"Career"}/>
+                <Section.Title title={"Software"}/>
+                <Section.SubTitle title={"Android"}/>
+                <Section.SubTitle title={"Web Application"}/>
+                <Section.SubTitle title={"Node.js"}/>
+                <Section.SubTitle title={"Atom"}/>
+                <Section.SubTitle title={"Linux"}/>
+                <Section.SubTitle title={"Shell Script"}/>
             </section>
         )
+    }
+});
+
+Section.Title = React.createClass({
+    render: function () {
+        const title = this.props.title;
+        return (
+            <h2>
+                <a className="anchor" href={"#" + title} aria-hidden="true">
+                    <span className="octicon octicon-link"></span>
+                </a>{title}
+            </h2>
+        )
+    }
+});
+
+Section.SubTitle = React.createClass({
+    render: function() {
+        const title = this.props.title;
+        return (
+            <h3>
+                <a className="anchor" href={"#" + title} aria-hidden="true">
+                    <span className="octicon octicon-link"></span>
+                </a>{title}
+            </h3>
+        )
+    }
+});
+
+Section.List = React.createClass({
+    render: function() {
+
+    }
+});
+
+Section.List.Item = React.createClass({
+    render: function() {
+
     }
 });
 
